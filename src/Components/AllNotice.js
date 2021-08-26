197180dcd93280a58f8ed47aeaa24c223ddb7fb7
mkonuk66 <mkonuk66@hotmail.com>
2021-08-26 10:53:31 +0300

    Event and Notice Detail page added. Edited styles.

---
+++ src/Components/AllNotice.js
@@ -0,0 +1,185 @@
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+
+export default class AllNotice extends Component {
+  render() {
+    return (
+      <div>
+        <div className="divSpace" />
+        <Container>
+          <h4 className="noticeTitle">Duyurular</h4>
+
+          <table className="notice">
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/ekim-ayi--siber-guvenlik-farkindalik-ayi,tr-2083.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <tr className="tr">
+                <td className="noticeDate">
+                  <cite>25</cite>
+                  <cite>Ağustos</cite>
+                </td>
+                <td>
+                  <p className="allNoticeP">
+                    Ekim Ayı – Siber Güvenlik Farkındalık Ayı
+                  </p>
+                </td>
+              </tr>
+            </a>
+
+            <br />
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/yozgat-bozok-universitesi-yeni-e-posta-sistemine-gecis-hakkinda-duzeltme,tr-2039.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <tr>
+                <td className="noticeDate">
+                  <cite>24</cite>
+                  <cite>Ağustos</cite>
+                </td>
+                <td>
+                  <p className="allNoticeP">
+                    Yozgat Bozok Üniversitesi Yeni E-Posta Sistemine Geçiş
+                    Hakkında (Düzeltme)
+                  </p>
+                </td>
+              </tr>
+            </a>
+
+            <br />
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+            <a
+              href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <tr>
+                <td className="noticeDate">
+                  <cite>23</cite>
+                  <cite>Ağustos</cite>
+                </td>
+
+                <td>
+                  <p className="allNoticeP">
+                    Üniversitemiz Antivirüs Programı Hakkında
+                  </p>
+                </td>
+              </tr>
+            </a>
+          </table>
+        </Container>
+      </div>
+    );
+  }
+}
--- src/Components/Notice.js
+++ src/Components/Notice.js
@@ -7,60 +7,67 @@ export default class Notice extends Component {
         <h4 className="noticeTitle">Duyurular</h4>
 
         <table className="notice">
-          <tr className="tr">
-            <td className="noticeDate">
-              <cite>25</cite>
-              <cite>Ağustos</cite>
-            </td>
-            <td>
-              <a
-                href="https://bozok.edu.tr/bidb/duyuru/ekim-ayi--siber-guvenlik-farkindalik-ayi,tr-2083.aspx"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Ekim Ayı – Siber Güvenlik Farkındalık Ayı
-              </a>
-            </td>
-          </tr>
-          <br />
-          <tr>
-            <td className="noticeDate">
-              <cite>24</cite>
-              <cite>Ağustos</cite>
-            </td>
-            <td>
-              <a
-                href="https://bozok.edu.tr/bidb/duyuru/yozgat-bozok-universitesi-yeni-e-posta-sistemine-gecis-hakkinda-duzeltme,tr-2039.aspx"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Yozgat Bozok Üniversitesi Yeni E-Posta Sistemine Geçiş Hakkında
-                (Düzeltme)
-              </a>
-            </td>
-          </tr>
-          <br />
-          <tr>
-            <td className="noticeDate">
-              <cite>23</cite>
-              <cite>Ağustos</cite>
-            </td>
+          <a
+            href="https://bozok.edu.tr/bidb/duyuru/ekim-ayi--siber-guvenlik-farkindalik-ayi,tr-2083.aspx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <tr className="tr">
+              <td className="noticeDate">
+                <cite>25</cite>
+                <cite>Ağustos</cite>
+              </td>
+              <td>
+                <p>Ekim Ayı – Siber Güvenlik Farkındalık Ayı</p>
+              </td>
+            </tr>
+          </a>
 
-            <td>
-              <a
-                href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Üniversitemiz Antivirüs Programı Hakkında
-              </a>
-            </td>
-          </tr>
+          <br />
+          <a
+            href="https://bozok.edu.tr/bidb/duyuru/yozgat-bozok-universitesi-yeni-e-posta-sistemine-gecis-hakkinda-duzeltme,tr-2039.aspx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <tr>
+              <td className="noticeDate">
+                <cite>24</cite>
+                <cite>Ağustos</cite>
+              </td>
+              <td>
+                <p>
+                  Yozgat Bozok Üniversitesi Yeni E-Posta Sistemine Geçiş
+                  Hakkında (Düzeltme)
+                </p>
+              </td>
+            </tr>
+          </a>
+
+          <br />
+          <a
+            href="https://bozok.edu.tr/bidb/duyuru/universitemiz-antivirus-programi-hakkinda,tr-2025.aspx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <tr>
+              <td className="noticeDate">
+                <cite>23</cite>
+                <cite>Ağustos</cite>
+              </td>
+
+              <td>
+                <p>Üniversitemiz Antivirüs Programı Hakkında</p>
+              </td>
+            </tr>
+          </a>
+
           <tr>
             <td></td>
             <td></td>
             <td>
-              <a href="*"> Daha fazlası </a>
+              <a href="/notice"> Daha fazlası </a>
             </td>
           </tr>
         </table>
