a4f7e75147c4ada8eab13cb818824427ebc973c1
mkonuk66 <mkonuk66@hotmail.com>
2021-09-22 09:26:08 +0300

    Slider db model created. Insert process completed.

--- src/Components/Admin/AdminNewSlider.js
+++ src/Components/Admin/AdminNewSlider.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
+import FileBase64 from "react-file-base64";
+import axios from "axios";
 
+let imagePath = "";
 export default class AdminNewSlider extends Component {
+  getFiles(files) {
+    imagePath = files;
+  }
+  handleSubmit(e) {
+    let date = new Date();
+    const newSlider = {
+      title: document.getElementById("title").value,
+      sliderImage: imagePath,
+      createdAt: `${date.getDate()}.${
+        date.getMonth() + 1
+      }.${date.getFullYear()}`,
+      updatedAt: `${date.getDate()}.${
+        date.getMonth() + 1
+      }.${date.getFullYear()}`,
+    };
+    console.log(newSlider);
+    axios
+      .post(
+        "https://mkonuk-intern-site.herokuapp.com/admin/sliders/newSlider",
+        newSlider
+      )
+      .then(
+        (res) => console.log(res.data),
+        alert("Slider başarıyla eklendi"),
+        window.open("/adminSlider", "_self")
+      )
+      .catch((err) => alert("Hata: " + err));
+  }
   render() {
     return (
       <div className="container mt-5">
@@ -16,10 +47,19 @@ export default class AdminNewSlider extends Component {
           </h2>
         </div>
         <form>
+          <div className="form-group mb-3">
+            <label className="mb-1">Slider Başlık</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="Slider başlık giriniz"
+            />
+          </div>
           <div className="form-group mt-4">
             <label className=" mb-1">Slider Fotoğraf</label>
             <br />
-            <input type="file" className="form-control-file" id="sliderImage" />
+            <FileBase64 multiple={false} onDone={this.getFiles.bind(this)} />
           </div>
           <a href="/adminSlider" class="btn btn-warning mb-2 mt-5">
             Slider Ekle
